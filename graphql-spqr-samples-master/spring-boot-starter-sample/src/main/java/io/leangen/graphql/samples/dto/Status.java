9b195bd8268c22ea8944e34258092107fc474d20
amine-hamzi <58113908+amine-hamzi@users.noreply.github.com>
2023-03-12 22:43:59 +0100

    Delete graphql-spqr-samples-master directory

--- graphql-spqr-samples-master/spring-boot-starter-sample/src/main/java/io/leangen/graphql/samples/dto/Status.java
+++
@@ -1,6 +0,0 @@
-package io.leangen.graphql.samples.dto;
-
-public enum Status {
-
-    PLANNING, READY, WIP, VERIFICATION, DONE
-}
--- graphql-spqr-samples-master/spring-boot-starter-sample/src/main/java/io/leangen/graphql/samples/dto/Type.java
+++
@@ -1,6 +0,0 @@
-package io.leangen.graphql.samples.dto;
-
-public enum Type {
-
-    PURCHASE, MAINTENANCE, CREATIVE, PRODUCTION
-}
